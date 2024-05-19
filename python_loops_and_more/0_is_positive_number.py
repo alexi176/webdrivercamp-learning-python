9f469033ab59babf1e1bbd66e91b4b4999d95053
Shakh Aleksei <shakh1972@gmail.com>
2024-05-19 12:59:43 -0700

    2.1 Python_loops_and_more

---
+++ python_loops_and_more/0_is_positive_number.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import random
+
+random_num = random.randint(-10, 10)
+
+if random_num < 0:
+     print(random_num,"is negative")
+elif random_num == 0:
+     print(random_num, "is zero")
+else:
+     print(random_num,"is positive")
